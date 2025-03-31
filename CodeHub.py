f4575e866546b02aa3c924d8385210170042a4ad
km772010 <speedkoko2010@gmail.com>
2025-03-31 23:57:12 +0400

    Add files via upload

---
+++ CodeHub.py
@@ -0,0 +1,183 @@
+import time
+import datetime
+import random
+
+x = datetime.datetime.now()
+Random = random.randint(1, 999999999)
+SecretInsaneDice = str(random.randint(100000000, 999999999, not 111222333))
+
+print("Hello Friend...")
+time.sleep(2)
+print("Here You Have To Enter A Random Code")
+time.sleep(2)
+print("But There Are Many Of Them...")
+time.sleep(2)
+a = input("Please Enter A Code:\n")
+
+
+
+if a == "666999":
+    print("Minecraft Server!")
+    print("IP: GameHub987.aternos.me")
+    print("Port: 45334")
+    print("Mini Game Hub!")
+    time.sleep(10)
+
+elif a == "1":
+    print("Full Tool Ai!")
+    print("Web: blackbox.ai")
+    print("Name: BlackBox Ai")
+    print("VSCode Included With Ai Powered Programming!")
+    time.sleep(10)
+
+elif a == "15":
+    print("Club: Real Madrid")
+    print("They Have 15 UCL!")
+    time.sleep(10)
+
+elif a == "000000":
+    print("Paid Games From Steam For Free!")
+    print("Web: https://www.steamunlocked.net")
+    print("All Games In One Place :)")
+    time.sleep(10)
+
+elif a == "99":
+    print("Free Anime Game!")
+    print("Name: StarBlast Anime War Mugen")
+    print("All Anime Characters In One Game")
+    time.sleep(10)
+
+elif a == "8810":
+    print("Co-op Horror Game!")
+    print("Name: The Ghost")
+    print("Platform: Android And iOS")
+    print("Size: Over 800 Mbs")
+    print("Maps: 4")
+    print("Monesters: N/A")
+    time.sleep(20)
+
+elif a == "7":
+    print("SUI!!!")
+    time.sleep(5)
+
+elif a == "128":
+    print("R.E.P.O Game On Android!")
+    print("Avaliablilty: Play Store")
+    print("Link: https://play.google.com/store/apps/details?id=com.sandswept.repo")
+    print("Ads: With Ads :(")
+    print("Online Mode: Avaliable!")
+    time.sleep(20)
+
+elif a == "1121":
+    print("DB LEGENDS")
+    print("Account: KMK")
+    print("Description: The Owner Of This Py File")
+    time.sleep(10)
+
+elif a == "111222333":
+    print(x)
+    time.sleep(5)
+
+elif a == "93":
+    print("Sergio Ramos...")
+    time.sleep(1)
+    print("Real Madrid Savior!")
+    time.sleep(10)
+
+elif a == "10":
+    print("Messi")
+    time.sleep(5)
+
+elif a == "999":
+    print("Sigma Sigma Boy!!")
+    time.sleep(1)
+    print("You Are Not Sigma.")
+    time.sleep(10)
+
+elif a == "666":
+    print("Guest666...")
+    time.sleep(2)
+    print("A Roblox Villian")
+    time.sleep(10)
+
+elif a == "001002":
+    print("N I G G A")
+    time.sleep(3)
+
+elif a == "856":
+    print("FC25 SHOWCASE")
+    time.sleep(2)
+    print("FC25 Demo And Its Called (Showcase)")
+    time.sleep(1)
+    print("Avaliablilty: Steam. PS And XBOX")
+    time.sleep(10)
+
+elif a == "X319S23O":
+    print("SECRET CODE...")
+    print("Rarity: Legendary")
+    print("Chance: %0.32")
+    print("You Have Unlocked A Secret Code!")
+    time.sleep(10)
+
+elif a == "123789":
+    print("Honor X7b")
+    time.sleep(5)
+
+elif a == "7788":
+    print("Your Number Is:"+ str(Random))
+    time.sleep(10)
+
+elif a == "0001000":
+    print("You Are A FUCKIN' DIRTY NIGGA")
+    time.sleep(5)
+
+elif a == "GITHUB":
+    print("Link Not Available")
+    print("Error: 404 (Not Found)")
+    time.sleep(5)
+
+elif a == "Help":
+    print("You Need To Type Any Number And Check If Its Available Or Not")
+    print("Thats All!")
+    time.sleep(5)
+
+elif a == "002":
+    print("Football Hunter...")
+    time.sleep(1)
+    print("Don Lorenzo!")
+    time.sleep(2)
+    print("Name: Don Lorenzo")
+    print("Style: Hunts Players Who Have Big Values (Example: Kaiser)")
+    print("Nationality: Italian")
+    print("Team: Blue Lock Juventus")
+    time.sleep(10)
+
+elif a == SecretInsaneDice:
+    print("Woah Man...")
+    time.sleep(3)
+    print("Secret Dice Roll!")
+    time.sleep(2)
+    print("Rarity: INSANE!")
+    time.sleep(10)
+
+elif a == "911":
+    print("An Airplane Hitted 2 Towers :)")
+    print("Note: Its Just For Fun")
+    time.sleep(4)
+
+elif a == "@":
+    print("Your Gf Need To Make You Happy!")
+    time.sleep(3)
+    print("But.. Sad :(")
+    time.sleep(3)
+    print("You Dont Have One")
+    time.sleep(10)
+
+elif a == "Free":
+    print("Nothing is Free Bro")
+    time.sleep(2)
+
+else:
+    print("Wrong Code!")
+    time.sleep(1)
+    a = input("Please Enter a Code:\n")
